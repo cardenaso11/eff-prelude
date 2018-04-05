db8227d3f1b580de700bd702f9ddc2d488586725
Nicholas Cardenas <nicholas.cardenas.1@gmail.com>
2018-04-05 01:19:11 -0700

    added random effect !

--- src/Control/Monad/Freer/Random.hs
+++ src/Control/Monad/Freer/Random.hs
@@ -1,1 +1,11 @@
-module Control.Monad.Freer.Random () where
+module Control.Monad.Freer.Random (Random(..), runRandom) where
+
+import Control.Monad.Freer as F
+import qualified Control.Monad.Freer.State as S
+import qualified System.Random as R
+
+data Random s r where
+    Get :: R.RandomGen s => Random s s
+
+runRandom :: s -> Eff (Random s : effs) x -> Eff effs x
+runRandom gen0 = S.evalState gen0 . translate (\Get -> S.Get)
